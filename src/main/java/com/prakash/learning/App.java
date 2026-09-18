21da32180e8fdd1fb04334f976870577533a4f3c
Prakash Singh <prakashsingh@Prakashs-MacBook-Pro.local>
2026-09-18 19:22:33 +0530

    Trivial change to exercise Multibranch Pipeline discovery

--- src/main/java/com/prakash/learning/App.java
+++ src/main/java/com/prakash/learning/App.java
@@ -8,7 +8,7 @@ public class App {
 
     public static void main(String[] args) {
         App app = new App();
-        System.out.println("Hello, Maven!");
+        System.out.println("Hello, Maven! (from feature/test-multibranch)");
         System.out.println("2 + 3 = " + app.add(2, 3));
     }
 }
