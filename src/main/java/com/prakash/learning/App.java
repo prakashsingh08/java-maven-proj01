b06de1482906f29f204454485d1950ab215da76f
Prakash Singh <prakashsingh@Prakashs-MacBook-Pro.local>
2026-09-19 07:34:20 +0530

    Trivial change for scenario A

--- src/main/java/com/prakash/learning/App.java
+++ src/main/java/com/prakash/learning/App.java
@@ -8,7 +8,7 @@ public class App {
 
     public static void main(String[] args) {
         App app = new App();
-        System.out.println("Hello, Maven!");
+        System.out.println("Hello, Maven! welcome scenario A");
         System.out.println("2 + 3 = " + app.add(2, 3));
     }
 }
